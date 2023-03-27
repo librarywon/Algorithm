61eb78d120763ca4860148cfd1b336d693bd5376
librarywon <52442547+librarywon@users.noreply.github.com>
2023-03-27 16:21:36 +0900

    [Silver III] Title: 통계학, Time: 476 ms, Memory: 53088 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,24 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+n =int(input())
+list = []
+D = defaultdict(int)
+for i in range(n):
+    N = int(input())
+    D[N]+=1
+    list.append(N)
+list.sort()
+print(int('{:.0f}'.format(sum(list)/n)))
+print(list[int(n/2)])
+list2=[]
+many = max(D.values())
+for k,v in D.items():
+    if many == v:
+        list2.append(k)
+list2.sort()
+if len(list2)>1:
+    print(list2[1])
+else:
+    print(list2[0])
+print(list[n-1]-list[0])
