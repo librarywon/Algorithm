5c8be783babacd5acf0f7225b900c00d2c430b1c
Jaewon Seo <52442547+librarywon@users.noreply.github.com>
2024-03-12 00:12:38 +0900

    [Silver I] Title: 안전 영역, Time: 320 ms, Memory: 117212 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -10,15 +10,14 @@ def bfs(starti,startj,height):
     Q.append((starti,startj))
     visited[starti][startj] = True
     
-    il = [-1,1,0,0]
-    jl = [0,0,-1,1]
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     
     while Q : 
         nodei,nodej = Q.popleft()
-        visited[nodei][nodej] = True
         for c in range(4) :
-            newi = nodei + il[c] 
-            newj = nodej + jl[c] 
+            newi = nodei + dx[c] 
+            newj = nodej + dy[c] 
             
             if (0 <= newi <= N-1) and (0 <= newj <= N-1) and graph[newi][newj] > height:
                 if not visited[newi][newj] :
@@ -31,15 +30,20 @@ N = int(input())
 graph = [list(map(int,input().split())) for y in range(N)]
 visited = []
 
-
+# 높이를 1~100 모두 체크
 for i in range(1,101):
-    visited = [[False for x in range(N)] for y in range(N)] 
     
+    # 방문 배열, 갯수 초기화
+    visited = [[False for x in range(N)] for y in range(N)] 
     cnt = 0
+    
     for j in range(0,N):
         for k in range(0,N):
+            # 높이 보다 크고 && 방문하지 않았으면? BFS 시작
             if graph[j][k] > i and not visited[j][k] :
                 cnt += bfs(j,k,i)
+                
+    #MAX값 갱
     if max < cnt :
         max = cnt
 
