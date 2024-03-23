b8781e3e3f1fe94d7b1db378d573ca59677fe89c
Jaewon Seo <52442547+librarywon@users.noreply.github.com>
2024-03-23 17:45:51 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+
+int main(){
+    fast_io
+    string s; cin >> s;
+    long long cnt=0;
+    
+    for (int i = 1; i <s.length(); i++) {
+        if(s[i]!=s[i-1]) cnt++;
+    }
+
+    if (cnt%2==0) {
+        cout<< cnt/2;
+    }else {
+        cout << (cnt+1)/2;
+    }
+}
