3f7d9917113259f89f34047ee53b8cbd12f736e6
Jaewon Seo <52442547+librarywon@users.noreply.github.com>
2024-03-25 23:46:53 +0900

    [Silver I] Title: 스타트링크, Time: 12 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+using namespace std;
+#define fast_io cin.tie(NULL);ios_base::sync_with_stdio(false);
+
+void bfs() {
+    int F, S, G, U ,D;
+    cin >> F >> S >> G >> U >> D;
+
+    vector<bool> visited(F+1,false);
+    queue<int> q;
+    int elv[2]= {U,-D};
+
+    q.push(S);
+    visited[S] = true;
+
+    int level =0;
+
+    while(!q.empty()){
+        int qSize = q.size();
+        for(int i=0; i<qSize;i++) {
+            int curr = q.front();
+            if(curr == G) {
+                cout << level;
+                return;
+            }
+            q.pop();
+            for(int go: elv){
+                if(!visited[curr+go] && curr+go >0 && curr+go<=F) {
+                    visited[curr+go] = true;
+                    q.push(curr+go);
+                }
+            }
+        }
+        level++;
+    }
+
+    cout << "use the stairs";
+
+}
+
+int main(){
+    fast_io
+
+    bfs();
+    return 0;
+}
