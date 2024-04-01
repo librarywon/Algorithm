f6c8bd3aed542eaf4bb346bee878e59f8a84d13d
Jaewon Seo <52442547+librarywon@users.noreply.github.com>
2024-04-02 00:46:43 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#include <numeric>
+using namespace std;
+#define fast_io cin.tie(NULL);ios_base::sync_with_stdio(false);
+
+int main(){
+    fast_io
+
+    while(1){
+        int n; cin >> n;
+        if(n==0){
+            return 0;
+        }
+
+        vector<int> a(n);
+        for(int i=0;i<n;i++) cin >> a[i];
+        sort(a.begin(),a.end());
+
+        vector<int> comb(n, 0);
+        fill(comb.begin(), comb.begin() + 6, 1);
+
+        do {
+            for(int i=0;i<n;i++){
+                if(comb[i]){
+                    cout << a[i] <<" ";
+                }
+            }
+            cout << "\n";
+        }while(prev_permutation(comb.begin(),comb.end()));
+        cout << "\n";
+    }
+}
